2930f0af72da49525b881b42d16117ffb91979d9
TheVaultCI236 <50559307+TheVaultCI236@users.noreply.github.com>
2019-05-13 00:19:33 +0100

    Add files via upload

---
+++ puzzle7.java
@@ -0,0 +1,18 @@
+
+public class puzzle7 {
+
+	
+	/**
+	 * Create a new variable a and multiply it by a 3.142.
+	 * 
+	 * @param args
+	 */
+		
+	public static void main(String args[]) {
+		
+		int a;
+		
+	   
+		
+	}
+}
